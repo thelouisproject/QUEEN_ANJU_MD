2ced301b42e7a40fcf319ef725fbeea3a4cba47a
thelouisproject <155336783+thelouisproject@users.noreply.github.com>
2024-11-17 16:33:32 +0100

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=XdNzhIIR#9yLuGFckaztc815suy5NgmBpB1bAnzhb8oDodTPzPX0",
 };
